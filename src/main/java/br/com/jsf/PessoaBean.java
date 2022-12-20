67c14bab2618674625184875486a5108f5c53f34
Pelegrino <pelegrino@gmail.com>
2022-12-20 14:49:49 -0300

    Initial commit

---
+++ src/main/java/br/com/jsf/PessoaBean.java
@@ -0,0 +1,25 @@
+package br.com.jsf;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "pessoaBean")
+public class PessoaBean {
+	
+	private String nome;
+	private String sobrenome;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+}
