9e8368b32f780a7e8d9bb782bbc680dbbe217d5e
Pelegrino <pelegrino@gmail.com>
2022-12-21 18:20:06 -0300

    Primeiros passos em JSF

--- src/main/java/br/com/jsf/PessoaBean.java
+++ src/main/java/br/com/jsf/PessoaBean.java
@@ -1,25 +1,38 @@
 package br.com.jsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
 @ManagedBean(name = "pessoaBean")
+@RequestScoped
 public class PessoaBean {
 	
 	private String nome;
-	private String sobrenome;
 	
+	private List<String> nomes = new ArrayList<String>();
+	
+	public String addNome() {
+		nomes.add(nome);
+		return "";
+	}
 	
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getSobrenome() {
-		return sobrenome;
+	
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
 	}
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
+	
+	public List<String> getNomes() {
+		return nomes;
 	}
 	
 }
