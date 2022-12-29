b0327d7b742933a4e027092088e22eeb37ed569b
Pelegrino <pelegrino@gmail.com>
2022-12-29 17:45:06 -0300

    DataTable - Carregando dados com JSF e JPA

--- src/main/java/br/com/dao/DaoGeneric.java
+++ src/main/java/br/com/dao/DaoGeneric.java
@@ -1,5 +1,7 @@
 package br.com.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
@@ -53,4 +55,15 @@ public class DaoGeneric<E> {
 		
 	}
 	
+	public List<E> getListEntity(Class<E> entidade) {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		List<E> retorno = entityManager.createQuery("from " + entidade.getName()).getResultList();
+		entityTransaction.commit();
+		entityManager.close();
+		
+		return retorno;
+	}
 }
--- src/main/java/br/com/jsf/PessoaBean.java
+++ src/main/java/br/com/jsf/PessoaBean.java
@@ -1,5 +1,9 @@
 package br.com.jsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -12,9 +16,11 @@ public class PessoaBean {
 	
 	private Pessoa pessoa = new Pessoa();
 	private DaoGeneric<Pessoa> daoGeneric = new DaoGeneric<Pessoa>();
+	private List<Pessoa> pessoas = new ArrayList<Pessoa>();
 	
 	public String salvar() {
 		pessoa = daoGeneric.merge(pessoa);
+		carregarPessoas();
 		return "";
 	}
 	
@@ -26,8 +32,16 @@ public class PessoaBean {
 	public String remove() {
 		daoGeneric.deletePorId(pessoa);
 		pessoa = new Pessoa();
+		carregarPessoas();
 		return "";
 	}
+	
+	@PostConstruct
+	public void carregarPessoas() {
+		pessoas = daoGeneric.getListEntity(Pessoa.class);
+	}
+	
+	
 
 	public Pessoa getPessoa() {
 		return pessoa;
@@ -45,5 +59,7 @@ public class PessoaBean {
 		this.daoGeneric = daoGeneric;
 	}
 	
-	
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
 }
