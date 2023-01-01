041cb80ca91bf909f962ce2b7e7405e8b51e718f
Pelegrino <pelegrino@gmail.com>
2023-01-01 11:53:33 -0300

    Transformando o cadastro de pessoa em usuário do sistema

--- src/main/java/br/com/entidades/Pessoa.java
+++ src/main/java/br/com/entidades/Pessoa.java
@@ -36,8 +36,37 @@ public class Pessoa implements Serializable{
 	
 	private Boolean ativo;
 	
+	private String login;
+	
+	private String senha;
+	
+	private String perfilUser;
 	
 	
+	public void setPerfilUser(String perfilUser) {
+		this.perfilUser = perfilUser;
+	}
+	
+	public String getPerfilUser() {
+		return perfilUser;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
 	}
