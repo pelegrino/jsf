6577275f9be5dd56a94c44b4c2af09578217bc70
Pelegrino <pelegrino@gmail.com>
2023-01-02 10:49:38 -0300

    Filter para autenticação de usuário logado

---
+++ src/main/java/br/com/filter/FilterAutenticacao.java
@@ -0,0 +1,53 @@
+package br.com.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import br.com.entidades.Pessoa;
+import br.com.jpautil.JPAUtil;
+
+@WebFilter(urlPatterns = {"/*"})
+public class FilterAutenticacao implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		JPAUtil.getEntityManager();
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		
+		Pessoa usuarioLogado = (Pessoa) session.getAttribute("usuarioLogado");
+		
+		String url = req.getServletPath();
+		
+		if (!url.equalsIgnoreCase("index.jsf") && usuarioLogado == null) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsf");
+			dispatcher.forward(request, response);
+			return;
+		
+		} else {
+			//Executa as ações do request e do response
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
