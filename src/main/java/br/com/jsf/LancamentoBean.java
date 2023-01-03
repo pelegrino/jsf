8aaf6b2e7d477e3578ab3f4d010c45b1c554b083
Pelegrino <pelegrino@gmail.com>
2023-01-03 10:56:29 -0300

    Finalizando o CRUD de lançamentos

--- src/main/java/br/com/jsf/LancamentoBean.java
+++ src/main/java/br/com/jsf/LancamentoBean.java
@@ -3,6 +3,7 @@ package br.com.jsf;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
@@ -11,6 +12,8 @@ import javax.faces.context.FacesContext;
 import br.com.dao.DaoGeneric;
 import br.com.entidades.Lancamento;
 import br.com.entidades.Pessoa;
+import br.com.repository.IDaoLancamento;
+import br.com.repository.IDaoLancamentosImpl;
 
 @ViewScoped
 @ManagedBean(name = "lancamentoBean")
@@ -19,6 +22,7 @@ public class LancamentoBean {
 	private Lancamento lancamento = new Lancamento();
 	DaoGeneric<Lancamento> daoGeneric = new DaoGeneric<Lancamento>();
 	private List<Lancamento> lancamentos = new ArrayList<Lancamento>();
+	private IDaoLancamento daoLancamento = new IDaoLancamentosImpl();
 	
 	
 	public String salvar() {
@@ -30,15 +34,29 @@ public class LancamentoBean {
 		lancamento.setUsuario(pessoaUser);
 		daoGeneric.salvar(lancamento);
 		
+		carregarLancamentos();
+		
 		return "";
 	}
 	
+	@PostConstruct
+	private void carregarLancamentos() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = context.getExternalContext();
+		Pessoa pessoaUser = (Pessoa) externalContext.getSessionMap().get("usuarioLogado");
+		lancamentos = daoLancamento.consultar(pessoaUser.getId());
+	}
+
 	public String novo() {
+		lancamento = new Lancamento();
 		return "";
 		
 	}
 	
 	public String remove() {
+		daoGeneric.deletePorId(lancamento);
+		lancamento = new Lancamento();
+		carregarLancamentos();
 		return "";
 	}
 	
