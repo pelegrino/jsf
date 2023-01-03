0bc0ea8e4955ae5d4fc1eb4ac14594a2c3c6e5cd
Pelegrino <pelegrino@gmail.com>
2023-01-03 17:04:16 -0300

    Aprendendo o SelectManyCheckbox

--- src/main/java/br/com/entidades/Pessoa.java
+++ src/main/java/br/com/entidades/Pessoa.java
@@ -44,6 +44,16 @@ public class Pessoa implements Serializable{
 	
 	private String nivelProgramador;
 	
+	private Integer[] linguagens;
+	
+	
+	public void setLinguagens(Integer[] linguagens) {
+		this.linguagens = linguagens;
+	}
+	
+	public Integer[] getLinguagens() {
+		return linguagens;
+	}
 	
 	public void setNivelProgramador(String nivelProgramador) {
 		this.nivelProgramador = nivelProgramador;
